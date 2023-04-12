8eb1bbbcd6cd0d294eb1b1233eade9ba57f6336e
David Li <davidli012345@gmail.com>
2023-04-12 07:50:10 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.friendlyuser.github.io',
+  base: "/astrobiz",
   integrations: [tailwind(), react()]
 });
